1271777f4d4415bdf0b85c2f270e3475b336cf93
LuisMi140905 <lperezacolt@gmail.com>
2026-03-06 10:55:15 -0600

    Tarea 2

---
+++ POO/Dispositivos.cs
@@ -0,0 +1,170 @@
+﻿
+// Programa principal
+
+
+
+
+
+//Interfaz
+public interface IDispositivoInteligente
+{
+    void Encender();
+    void Apagar();
+    void MoestrarEstado();
+
+}
+public class Lampara : IDispositivoInteligente //Brillo
+{
+    //Atributo
+    public int NivelBrillo {  get; set; }
+   public bool estado = false;
+    //Constructor
+    public Lampara(int  nivelBrillo)
+    {
+        NivelBrillo = nivelBrillo;
+        
+    }
+    //Metodo
+    public void Encender()
+    {
+      if (!estado)
+        {
+            estado = true;
+            Console.WriteLine("La lampara se encendio");
+        }
+      else
+        {
+            Console.WriteLine("la lampara ya estaba encendida");
+        }
+    }
+    public void Apagar()
+    {
+        if (!estado)
+        {
+            estado = false;
+            Console.WriteLine("La lampara se apago");
+        }
+        else
+        {
+            Console.WriteLine("La lampara ya estaba apagada");
+        }
+    }
+    public void MoestrarEstado()
+    {
+        if (estado == true)
+        {
+            Console.WriteLine($"La lampara esta encendida con un nivel de brillo de: {NivelBrillo}");
+        }
+        else
+        {
+            Console.WriteLine("Tu lampara esta apagada");
+        }
+
+    }
+
+    public void ControlBrillo(int nivelBrillo)
+    {
+        Console.WriteLine("Puedes ajustar el brillo entre 1 a 10");
+        Console.WriteLine($"Deseas cambiar el brillo, el brillo actual es {NivelBrillo}, presiona S si es afirmativo");
+        char Br = char.Parse(Console.ReadLine() ?? "".ToLower());
+
+    }
+}
+public class Vetilador : IDispositivoInteligente//Velocidad
+{
+    //Atributo
+    public int Velocidad {  get; set; }
+    public bool estadoV = false;
+
+    //Constructor
+    public Vetilador(int velocidad)
+    {
+        Velocidad = velocidad;
+    }
+    //Metodo
+    public void Encender()
+    {
+        if (!estadoV)
+        {
+            estadoV = true;
+            Console.WriteLine("El ventilador se encendio");
+        }
+        else
+        {
+            Console.WriteLine("El ventilador ya estaba encendido");
+        }
+    }
+    public void Apagar()
+    {
+        if (!estadoV)
+        {
+            estadoV = false;
+            Console.WriteLine("El ventilador se apago");
+        }
+        else
+        {
+            Console.WriteLine("El ventilador ya estaba apagado");
+        }
+    }
+    public void MoestrarEstado()
+    {
+        if (estadoV == true)
+        {
+            Console.WriteLine($"El ventilador esta encendido con un nivel de velocidad: {Velocidad}");
+        }
+        else
+        {
+            Console.WriteLine("Tu ventilador esta apagado");
+        }
+
+    }
+}
+public class Altavoz : IDispositivoInteligente //Reproducir musica
+{
+    //Atributo
+    public string ReproducirMusica {  get; set; }
+    public bool estadoA;
+    //Constructor
+    public Altavoz (string reproducirMusica)
+    {
+        ReproducirMusica = reproducirMusica;
+    }
+    public void Encender()
+    {
+        if (!estadoA)
+        {
+            estadoA = true;
+            Console.WriteLine("El altavoz se encendio");
+        }
+        else
+        {
+            Console.WriteLine("El altavoz ya estaba encendido");
+        }
+    }
+    public void Apagar()
+    {
+        if (!estadoA)
+        {
+            estadoA = false;
+            Console.WriteLine("El altavoz se apago");
+        }
+        else
+        {
+            Console.WriteLine("El altavoz ya estaba apagado");
+        }
+    }
+    //Metodo
+    public void MoestrarEstado()
+    {
+        if (estadoA == true)
+        {
+            Console.WriteLine($"Tu altavoz esta encendido y esta reproduciendo: {ReproducirMusica}");
+        }
+        else
+        {
+            Console.WriteLine("Tu altavoz esta apagado");
+        }
+
+    }
+
+}
---
+++ POO/Pagos.cs
@@ -0,0 +1,123 @@
+﻿//Programa principal
+bool continuar = true;
+List <IPago> listaPagos = new List <IPago> ();
+do
+{
+    Console.WriteLine("Ingresa el monto a pagar");
+   
+        string montoTexto = Console.ReadLine() ?? "";
+        if(double.TryParse(montoTexto,out double monto))
+        {
+            string modoPagoT;
+            int modoPago;
+            do
+            {
+                Console.WriteLine("1- Pago con tarjeta");
+                Console.WriteLine("2- Pago en efectivo");
+
+                modoPagoT = Console.ReadLine() ?? "";
+
+            } while (!int.TryParse(modoPagoT, out modoPago) || (modoPago !=2 && modoPago !=1));
+
+            if (modoPago == 1)
+            {
+                Console.WriteLine("Ingresa el umero de tarjeta: ");
+                string tarjeta = Console.ReadLine() ?? "";
+
+                //creando objeto para pago con tarjeta
+
+                IPago pago = new PagoTarjeta(tarjeta,monto);
+                listaPagos.Add (pago);
+            }
+            else 
+            {
+                //creando objeto para pago con efectivo
+
+                IPago pago = new PagoEfectivo( monto);
+                listaPagos.Add(pago);
+            }
+
+        }
+       else
+        {
+            Console.WriteLine("Moto invalido");
+            return;
+        }
+        
+    
+
+    Console.WriteLine("Presiona S para procesar más pagos: ");
+    char continuaT = char.Parse(Console.ReadLine() ?? "".ToLower());
+    if(continuaT == 's')
+    {
+        continuar = true;
+    }
+    else
+    {
+        continuar = false; 
+    }
+}
+while (continuar);
+
+foreach (IPago pago in listaPagos) //Tipo de dato, variable temporal en lista declarada
+{
+    pago.ProcesarPago();
+}
+
+//Interfaz y clases
+
+public interface IPago
+{
+    void ProcesarPago();
+}
+
+//Clase de pago en efectivo
+
+public class PagoEfectivo : IPago
+{
+    //Atributo
+    public double Monto { get; set; }
+
+    //Constructor
+    public PagoEfectivo(double monto)
+    {
+        Monto = monto;
+    }
+
+    //Metodos
+    public void ProcesarPago()
+    {
+        Console.WriteLine($"Pago en efectivo de {Monto} proccesado");
+    }
+}
+
+public class PagoTarjeta : IPago
+{
+    //Atributo
+    public double Monto { get; set; }
+    public string NumTarjeta {  get; set; }
+
+
+    //Constructor
+    public PagoTarjeta(string numTarjeta,double monto)
+    {
+        Monto = monto;
+        NumTarjeta = numTarjeta;    
+    }
+
+
+    //Metodos
+    public void ProcesarPago()
+    {
+        if (NumTarjeta.Length == 16)
+        {
+            Console.WriteLine($"Pago en tarjeta de {Monto} proccesado");
+        }
+        else
+        {
+            Console.WriteLine($"Tarjeta invalida");
+        }
+    }
+}
+
+
